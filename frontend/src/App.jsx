fc61b4e35e80158e9e01ccdecc3f1ea1c4f2e25d
Julie Combeau <combeau.julie@gmail.com>
2022-07-20 11:53:26 +0200

    add routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import GameDetails from "./pages/GameDetails";
+import Register from "./pages/Register";
+import LoginForm from "./pages/LoginForm";
+import AdministratorDashboard from "./pages/AdministratorDashboard";
+import Error from "./pages/Error";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/détails/:gameId" element={<GameDetails />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route
+          path="/dashboardAdministrator"
+          element={<AdministratorDashboard />}
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
   );
 }
